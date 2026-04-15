3c0ed1f82992d2162a1f12696a1e3facd21deef4
zhangjh100 <zhangjh100@nenu.edu.cn>
2026-04-15 12:31:26 +0800

    update 1

--- models/WaveCo_BraTS.py
+++ models/WaveCo_BraTS.py
@@ -253,17 +253,49 @@ class intraInteraction(nn.Module):
         h2 = self.attention2(x2)
         return h1, h2
 
+
+class LayerNormFunction(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, x, weight, bias, eps):
+        ctx.eps = eps
+        N, C, D, H, W = x.size()
+
+        mu = x.mean(1, keepdim=True)
+        var = (x - mu).pow(2).mean(1, keepdim=True)
+        y = (x - mu) / (var + eps).sqrt()
+
+        ctx.save_for_backward(y, var, weight)
+
+        y = weight.view(1, C, 1, 1, 1) * y + bias.view(1, C, 1, 1, 1)
+        return y
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        eps = ctx.eps
+
+        N, C, D, H, W = grad_output.size()
+        y, var, weight = ctx.saved_variables
+
+        g = grad_output * weight.view(1, C, 1, 1, 1)
+        mean_g = g.mean(dim=1, keepdim=True)
+        mean_gy = (g * y).mean(dim=1, keepdim=True)
+
+        gx = 1. / torch.sqrt(var + eps) * (g - y * mean_gy - mean_g)
+
+        return gx, (grad_output * y).sum(dim=4).sum(dim=3).sum(dim=2).sum(dim=0), grad_output.sum(dim=4).sum(dim=3).sum(
+            dim=2).sum(dim=0), None
+
 class LayerNorm3d(nn.Module):
+
     def __init__(self, channels, eps=1e-6):
-        super().__init__()
-        self.weight = nn.Parameter(torch.ones(1, channels, 1, 1, 1))
-        self.bias = nn.Parameter(torch.zeros(1, channels, 1, 1, 1))
+        super(LayerNorm3d, self).__init__()
+        self.register_parameter('weight', nn.Parameter(torch.ones(channels)))
+        self.register_parameter('bias', nn.Parameter(torch.zeros(channels)))
         self.eps = eps
 
     def forward(self, x):
-        mean = x.mean(dim=1, keepdim=True)
-        var = x.var(dim=1, keepdim=True, unbiased=False)
-        return (x - mean) / torch.sqrt(var + self.eps) * self.weight + self.bias
+        return LayerNormFunction.apply(x, self.weight, self.bias, self.eps)
 
 
 class LWN3D(nn.Module):
